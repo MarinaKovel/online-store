567d9c156b4bce82d59296734310423ba381b685
Aliaksandr Konik <cibulyadev@gmail.com>
2023-01-09 02:02:44 +0100

    fix:fix 404bug

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import './_nullstyle.scss';
 import './global.scss';
 import App from './controller/app/app';
+
+localStorage.setItem('last-view', 'main-page');
 window.location.hash = `${localStorage.getItem('last-view')}`;
+
 const app = new App();
 app.run();
